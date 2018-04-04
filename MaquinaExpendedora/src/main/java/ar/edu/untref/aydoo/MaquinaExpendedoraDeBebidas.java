50bcfbcadd3c595f12f7dff20d6bb773c9b4ceaf
Martin <martincapocasale@gmail.com>
2018-04-03 22:27:46 -0300

    Se agrega clase Maquina y Vaso que necesitan los test

---
+++ MaquinaExpendedora/src/main/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidas.java
@@ -0,0 +1,11 @@
+
+public class MaquinaExpendedoraDeBebidas {
+
+	public void hacerCafeConLecheConNDeAzucar(Vaso unVaso, int cantidadAzucar) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
---
+++ MaquinaExpendedora/src/main/java/ar/edu/untref/aydoo/Vaso.java
@@ -0,0 +1,21 @@
+
+public class Vaso {
+
+	
+	
+	public Object getCantidadDeAzucar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean tieneCafe() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean tieneAzucar() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
