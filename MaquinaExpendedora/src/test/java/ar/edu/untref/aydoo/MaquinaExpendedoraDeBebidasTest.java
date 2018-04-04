28203ef69a88134f99c910830bda42cacad0f50b
Martin <martincapocasale@gmail.com>
2018-04-03 22:23:04 -0300

    Se agrega Repositorio Con Test

---
+++ MaquinaExpendedora/src/test/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidasTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+class MaquinaExpendedoraDeBebidasTest {
+    @Test
+    public void deberiaHacerCafeConLecheCon10DeAzucar() {
+        int cantidadAzucar = 10;
+        Vaso unVaso = new Vaso();
+        MaquinaExpendedoraDeBebidas maquinaExpendedoraDeBebidas = new MaquinaExpendedoraDeBebidas();
+        
+        maquinaExpendedoraDeBebidas.hacerCafeConLecheConNDeAzucar(unVaso, cantidadAzucar);
+
+        Assert.assertEquals(cantidadAzucar, unVaso.getCantidadDeAzucar());
+        Assert.assertTrue(unVaso.tieneCafe());
+        Assert.assertTrue(unVaso.tieneAzucar());
+        Assert.assertFalse(unVaso.tieneCafe());
+    }
+
+    @Test
+    public void deberiaHacerTeConLecheCon0DeAzucar() {
+
+        int cantidadAzucar = 0;
+        Vaso unVaso = new Vaso();
+        MaquinaExpendedoraDeBebidas maquinaExpendedoraDeBebidas = new MaquinaExpendedoraDeBebidas();
+
+        maquinaExpendedoraDeBebidas.hacerCafeConLecheConNDeAzucar(unVaso, cantidadAzucar);
+
+        Assert.assertEquals(cantidadAzucar, unVaso.getCantidadDeAzucar());
+        Assert.assertFalse(unVaso.tieneCafe());
+        Assert.assertFalse(unVaso.tieneAzucar());
+        Assert.assertTrue(unVaso.tieneCafe());
+    }
+}
