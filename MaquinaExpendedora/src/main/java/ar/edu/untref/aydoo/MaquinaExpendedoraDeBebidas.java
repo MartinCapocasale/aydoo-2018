92078230551a384aad146a3e6537fe8b3912d1dd
Martin <martincapocasale@gmail.com>
2018-04-03 23:54:11 -0300

    Se modifica clase vaso y maquina

--- MaquinaExpendedora/src/main/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidas.java
+++ MaquinaExpendedora/src/main/java/ar/edu/untref/aydoo/MaquinaExpendedoraDeBebidas.java
@@ -1,8 +1,17 @@
 
 public class MaquinaExpendedoraDeBebidas {
-
+	
+	
 	public void hacerCafeConLecheConNDeAzucar(Vaso unVaso, int cantidadAzucar) {
-		// TODO Auto-generated method stub
+		unVaso.agregarSustancia("cafe");
+		unVaso.agregarSustancia("leche");
+		unVaso.agregarNDeAzucar(cantidadAzucar);		
+		
+	}
+	public void hacerTeConLecheConNDeAzucar(Vaso unVaso, int cantidadAzucar) {
+		unVaso.agregarSustancia("te");
+		unVaso.agregarSustancia("leche");
+		unVaso.agregarNDeAzucar(cantidadAzucar);
 		
 	}
 
--- MaquinaExpendedora/src/main/java/ar/edu/untref/aydoo/Vaso.java
+++ MaquinaExpendedora/src/main/java/ar/edu/untref/aydoo/Vaso.java
@@ -1,21 +1,58 @@
 
 public class Vaso {
 
+	private String sustancia;
+	private int cantidadDeAzucar;
 	
 	
-	public Object getCantidadDeAzucar() {
-		// TODO Auto-generated method stub
-		return null;
+	public int getCantidadDeAzucar() {
+		return cantidadDeAzucar ;
 	}
-
-	public boolean tieneCafe() {
-		// TODO Auto-generated method stub
-		return false;
+	
+	public void agregarNDeAzucar(int cantidad){
+		this.cantidadDeAzucar = cantidad;
 	}
 
 	public boolean tieneAzucar() {
-		// TODO Auto-generated method stub
-		return false;
+		boolean tieneAzucar = false;
+		if(this.cantidadDeAzucar > 0){
+			tieneAzucar = true;
+		}
+		return tieneAzucar;
+	}
+		
+	public void agregarSustancia(String unaSustancia){
+		this.sustancia = unaSustancia;
+		
+	}
+	
+	public String getSustancia(){
+		return this.sustancia;
+	}
+	
+	
+	public boolean tieneCafe() {
+		boolean tieneCafe = false;
+		if(this.sustancia == "cafe" ){
+			tieneCafe = true;
+		}
+		return tieneCafe;
+	}
+
+	public boolean tieneTe() {
+		boolean tieneTe = false;
+		if(this.sustancia == "te" ){
+			tieneTe = true;
+		}
+		return tieneTe;
+	}
+	
+	public boolean tieneLeche() {
+		boolean tieneLeche = false;
+		if(this.sustancia == "leche" ){
+			tieneLeche = true;
+		}
+		return tieneLeche;
 	}
 
 }
